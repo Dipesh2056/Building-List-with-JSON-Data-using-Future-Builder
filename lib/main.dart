eebf75d2666360a30d6e08d96b9ba880fda8fc52
Dipesh <dipeshsedhai1234@gmail.com>
2023-06-11 17:54:13 +0545

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'example_two.dart';
 import 'home_screen.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: ExampleTwo(),
     );
   }
 }
